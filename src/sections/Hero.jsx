b05697be239c146de7b80cb982eb4e57c3fd452e
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-26 13:32:11 +0300

    added footer/fix hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,11 +14,11 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -31,8 +31,8 @@ const Hero = () => {
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics.map((stat) => (
-            <div key={stat.label}>
+          {statistics.map((stat, index) => (
+            <div key={index}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
               <p className="leading-7 font-montserrat text-slate-gray">
                 {stat.label}
@@ -50,10 +50,11 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-          {shoes.map((shoe) => (
-            <div key={shoe}>
+          {shoes.map((image, index) => (
+            <div key={index}>
               <ShoeCard
-                imgURL={shoe}
+                index={index}
+                imgURL={image}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
               />
